726d9b0ccead671cdf028ae5e3d7ce1e1f35d55c
leexizhe <leexizhe17@gmail.com>
2023-09-01 23:02:01 +0800

    more JS

--- javascript/input.js
+++ javascript/input.js
@@ -32,3 +32,21 @@ document.getElementById("calculatorButton").onclick = function(){
 
     document.getElementById("cLabel").innerHTML = "Side C: " + c;
 }
+
+/*Counter program in JavaScript*/
+let count = 0;
+
+document.getElementById("decrease").onclick = function () {
+    count -= 1;
+    document.getElementById("countLabel").innerHTML = count;
+};
+
+document.getElementById("increase").onclick = function () {
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;
+};
+
+document.getElementById("reset").onclick = function () {
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+};
