fdc79fa99da2ab5cadcee65bf1a446c46fad063c
leexizhe <leexizhe17@gmail.com>
2023-09-05 19:14:49 +0800

    more JS

--- javascript/basic.js
+++ javascript/basic.js
@@ -22,9 +22,38 @@ console.log("BroCode - String slicing in JavaScript");
 let fullName = "Snoop Dogg";
 let firstName;
 let lastName;
-
 firstName = fullName.slice(0, fullName.indexOf(" "));
-lastName = fullName.slice(fullName.indexOf(" ") + 1);
 
+lastName = fullName.slice(fullName.indexOf(" ") + 1);
 console.log(firstName);
+
 console.log(lastName);
+
+/*JavaScript function expressions*/
+// function expression = function without a name (anonymous function) avoid polluting the global scope with names write it, then forget about it
+console.log("BroCode - JavaScript function expressions");
+const greeting = function () {
+
+    console.log("hello");
+}
+greeting();
+let count = 0;
+
+document.getElementById("increaseButton").onclick = function () {
+
+    count += 1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+document.getElementById("decreaseButton").onclick = function () {
+    count -= 1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+
+/*JavaScript arrow function expressions*/
+console.log("BroCode - JavaScript arrow function expressions");
+const hello = (userName) => console.log(`Hello ${userName}`)
+
+hello("world");
+
+const percent = (x, y) => x / y * 100;
+console.log(`${percent(1, 10)}%`)
