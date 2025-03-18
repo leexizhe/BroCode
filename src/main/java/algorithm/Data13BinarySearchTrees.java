d38dfa3859e08b84e8ddab3aca532a87b7f17be8
leexizhe <leexizhe17@gmail.com>
2025-03-18 22:19:28 +0800

    update

--- src/main/java/algorithm/Data13BinarySearchTrees.java
+++ src/main/java/algorithm/Data13BinarySearchTrees.java
@@ -47,6 +47,9 @@ public class Data13BinarySearchTrees {
         tree.insert(new Node(8));
 
         tree.display();
+        tree.search(2);
+        tree.remove(2);
+        tree.search(2);
     }
 
     Node root;
@@ -61,16 +64,12 @@ public class Data13BinarySearchTrees {
         }
     }
 
-    // insertion method
     public void insert(Node node) {
-
         root = insertHelper(root, node);
     }
 
     private Node insertHelper(Node root, Node node) {
-
         int data = node.data;
-
         if (root == null) {
             root = node;
             return root;
@@ -79,21 +78,83 @@ public class Data13BinarySearchTrees {
         } else {
             root.right = insertHelper(root.right, node);
         }
-
         return root;
     }
 
-    // display method
     public void display() {
         displayHelper(root);
     }
 
     private void displayHelper(Node root) {
-
         if (root != null) {
             displayHelper(root.left);
             System.out.println(root.data);
             displayHelper(root.right);
         }
+    }
+
+    public boolean search(int data) {
+        return searchHelper(root, data);
+    }
+
+    private boolean searchHelper(Node root, int data) {
+        if (root == null) {
+            System.out.println(data + " is not found");
+            return false;
+        } else if (root.data == data) {
+            System.out.println(data + " is found");
+            return true;
+        } else if (root.data > data) {
+            return searchHelper(root.left, data);
+        } else {
+            return searchHelper(root.right, data);
+        }
+    }
+
+    public void remove(int data) {
+
+        if (search(data)) {
+            removeHelper(root, data);
+        } else {
+            System.out.println(data + " could not be found");
+        }
+    }
+
+    private Node removeHelper(Node root, int data) {
+
+        if (root == null) {
+            return root;
+        } else if (data < root.data) {
+            root.left = removeHelper(root.left, data);
+        } else if (data > root.data) {
+            root.right = removeHelper(root.right, data);
+        } else { // node found
+            if (root.left == null && root.right == null) {
+                root = null;
+            } else if (root.right != null) {
+                root.data = successor(root);
+                root.right = removeHelper(root.right, root.data);
+            } else {
+                root.data = predecessor(root);
+                root.left = removeHelper(root.left, root.data);
+            }
+        }
+        return root;
+    }
+
+    private int successor(Node root) {
+        root = root.right;
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root.data;
+    }
+
+    private int predecessor(Node root) {
+        root = root.left;
+        while (root.right != null) {
+            root = root.right;
+        }
+        return root.data;
     }
 }
